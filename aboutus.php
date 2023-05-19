16677e6c46d3b07a70c951e6dd1897b367a7487f
qlabrys <qlabrys@gmail.com>
2023-05-19 18:17:07 +0800

    added about us page + local css/images

---
+++ aboutus.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About the Team</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-about.css">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-custom navbar-expand-lg">
+        <div class="container-fluid">
+            <img src="images/TUSTOS ICON NO BG.png" style="width: 3%; height: auto;" alt="Tustos Logo">
+          <a class="navbar-brand text-light" href="#">TUSTOS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="aboutus.html">About Us</a>
+              </li>
+            </ul>
+            <a href="#" class="btn btn-light btn-outline-dark">Log In</a>
+        </div>
+      </nav>
+
+    <div class="about-us p-5 d-flex justify-content-center">
+        <div class="box">
+        <h1>About Us</h1>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center p-4">
+    <img src="images/v5_45.png" class="img-fluid" alt="Header Image">
+    </div>
+
+    <div class="container text-center p-4 px-5">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="p-2 card bg1">
+                    <h3>Mission of TUSTOS</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+            </div>
+            <div class="col">
+                <div class="p-2 card bg1">
+                    <h3>How TUSTOS was formed</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="accordion justify-content-center w-75 mx-auto p-4">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Frequently Asked Questions
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>How do we do this?</strong> i dont know
+              <br>
+              <strong>Question 2?</strong> answer 2 
+            </div>
+          </div>
+        </div>
+    </div>
+    
+
+    <div class="container text-center">
+        <div class="diamond">
+        <div class="team-member">
+            <div class="card">
+            <img src="rico.jpg" alt="Team Member 1">
+            <h3>Rico</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="images/v5_45.png" class="center" alt="Team Member 2">
+            <h3>Nike</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="skychi.jpg" alt="Team Member 3">
+            <h3>Skychi</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="pique.jpg" alt="Team Member 4">
+            <h3>Pique</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    </body>
+
+    <footer>
+        <p>&copy; 2023 TUSTOS</p>
+    </footer>
+    </html>
