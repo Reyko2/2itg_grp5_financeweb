82581d9fe419eba2b4f7f8658af1dca80d427005
NicholasQuirky <124254381+NicholasQuirky@users.noreply.github.com>
2023-05-23 02:06:34 +0800

    Fixed extra text

--- login.php
+++ login.php
@@ -68,10 +68,7 @@ if (isset($_POST['email'])) {
 									  <input type="checkbox" checked>
 									  <span class="checkmark"></span>
 									</label>
-								</div>
-								<div class="w-50 text-md-right">
-									<a href="#">Forgot Password</a>
-								</div>
+								</div>							
 	            </div>
 	            <div class="form-group">
 	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" style="border-radius:0%">Login</button>
