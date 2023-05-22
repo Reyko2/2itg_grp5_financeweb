780388741c45395f68e524f4957921d3382528e8
qlabrys <qlabrys@gmail.com>
2023-05-22 23:49:42 +0800

    Added about us page to main branch

---
+++ aboutus.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About the Team</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-about.css">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-custom navbar-expand-lg">
+        <div class="container-fluid">
+            <img src="assets/img/TUSTOS ICON.png" style="width: 3%; height: auto;" alt="Tustos Logo">
+          <a class="navbar-brand text-light" href="#">TUSTOS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-light" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="aboutus.php">About Us</a>
+              </li>
+            </ul>
+            <a href="login.php" class="btn btn-light btn-outline-dark">Log In</a>
+        </div>
+      </nav>
+
+    <div class="about-us p-5 d-flex justify-content-center">
+        <div class="box">
+        <h1>About Us</h1>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center p-4">
+    <img src="assets/img/v5_45.png" class="img-fluid" alt="Header Image">
+    </div>
+    
+    <div class="accordion justify-content-center w-75 mx-auto p-4">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Frequently Asked Questions
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+
+            <div class="accordion-body">
+              <strong>What is TUSTOS?</strong>
+              <br> - TUSTOS: Trackable User Spending Through Online Service is a comprehensive financial tracking website that offers users various tools to manage their finances.
+              <br>
+              <br>
+              <strong>What are the features of TUSTOS?</strong>
+              <br> - TUSTOS currently has the following features available:
+                <ul>
+                  <li>A <b>Tracking</b> feature that monitors expenses.</li>
+                  <li>A <b>Financial Guide</b> that offers financial advice based on user data.</li>
+                  <li>The <b>Budget Manager</b> feature which aids in financial decisions.</li>
+                  <li>The <b>Reports</b> feature provides the user with detailed reports of their spending habits, income, and savings.</li>
+                  <li>The <b>Bill Tracking</b> feature that allows users to track their upcoming bills and dues to avoid late payments.</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+    </div>
+    
+
+    <div class="container text-center">
+        <div class="diamond">
+        <div class="team-member">
+            <div class="card">
+            <img src="assets/img/riconieto.png" alt="Team Member 1">
+            <h3>Rico Nieto</h3>
+              <br>
+              <br>
+            <h4>Chief Executive Officer, Developer</h4>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="assets/img/v5_45.png" class="center" alt="Team Member 2">
+            <h3>Nicholas Kerky Victoriano</h3>
+            <br>
+            <br>
+            <h4>Chief Financial Officer, Developer</h4>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="assets/img/jmevangelista.png" alt="Team Member 3">
+            <h3>Juan Miguel Evangelista</h3>
+            <br>
+            <br>
+            <h4>Chief Operating Officer, Developer</h4>
+            </div>
+        </div>
+        <div class="team-member">
+            <div class="card">
+            <img src="assets/img/enriqueresurreccion.jpg" alt="Team Member 4">
+            <h3>Enrique Resurreccion</h3>
+            <br>
+            <br>
+            <h4>Chief Marketing Officer, Developer, Representative</h4>
+            </div>
+        </div>
+        </div>
+    </div>
+    </body>
+
+    <footer>
+        <p>&copy; 2023 TUSTOS, a Project by 2ITG</p>
+    </footer>
+    </html>
