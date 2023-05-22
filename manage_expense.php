4bfc0ffcac46b36e816e11a1c07244ac488a07ba
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-05-22 18:36:57 +0800

    front end for sidebar addedd colors

--- manage_expense.php
+++ manage_expense.php
@@ -31,7 +31,7 @@ $exp_fetched = mysqli_query($con, "SELECT * FROM expenses WHERE user_id = '$user
     <div class="d-flex" id="wrapper">
 
         <!-- Sidebar -->
-        <div class="border-right" id="sidebar-wrapper">
+        <div class="border-right" id="sidebar-wrapper" style="background-color:#e1ffff">
             <div class="user">
                 <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="120">
                 <h5><?php echo $username ?></h5>
@@ -39,14 +39,14 @@ $exp_fetched = mysqli_query($con, "SELECT * FROM expenses WHERE user_id = '$user
             </div>
             <div class="sidebar-heading">Management</div>
             <div class="list-group list-group-flush">
-                <a href="index.php" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
-                <a href="add_expense.php" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
-                <a href="manage_expense.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="dollar-sign"></span> Manage Expenses</a>
+                <a href="index.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
+                <a href="add_expense.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
+                <a href="manage_expense.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action sidebar-active"><span data-feather="dollar-sign"></span> Manage Expenses</a>
             </div>
             <div class="sidebar-heading">Settings </div>
             <div class="list-group list-group-flush">
-                <a href="profile.php" class="list-group-item list-group-item-action "><span data-feather="user"></span> Profile</a>
-                <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
+                <a href="profile.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="user"></span> Profile</a>
+                <a href="logout.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
             </div>
         </div>
         <!-- /#sidebar-wrapper -->
--- profile.php
+++ profile.php
@@ -70,22 +70,22 @@ if (isset($_POST['but_upload'])) {
     <div class="d-flex" id="wrapper">
 
         <!-- Sidebar -->
-        <div class="border-right" id="sidebar-wrapper">
+        <div class="border-right" id="sidebar-wrapper" style="background-color:#e1ffff">
             <div class="user">
                 <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="120">
                 <h5><?php echo $username ?></h5>
                 <p><?php echo $useremail ?></p>
             </div>
-            <div class="sidebar-heading">Management</div>
+            <div class="sidebar-heading" style="background-color:#e1ffff">Management</div>
             <div class="list-group list-group-flush">
-                <a href="index.php" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
-                <a href="add_expense.php" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
-                <a href="manage_expense.php" class="list-group-item list-group-item-action "><span data-feather="dollar-sign"></span> Manage Expenses</a>
+                <a href="index.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action"><span data-feather="home"></span> Dashboard</a>
+                <a href="add_expense.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
+                <a href="manage_expense.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="dollar-sign"></span> Manage Expenses</a>
             </div>
-            <div class="sidebar-heading">Settings </div>
+            <div class="sidebar-heading" style="background-color:#e1ffff">Settings </div>
             <div class="list-group list-group-flush">
-                <a href="profile.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="user"></span> Profile</a>
-                <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
+                <a href="profile.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action sidebar-active"><span data-feather="user"></span> Profile</a>
+                <a href="logout.php" style="background-color:#e1ffff" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
             </div>
         </div>
         <!-- /#sidebar-wrapper -->
