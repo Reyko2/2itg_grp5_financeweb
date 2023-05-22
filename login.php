2963cc350739538db88d5722563e1446e374665b
NicholasQuirky <124254381+NicholasQuirky@users.noreply.github.com>
2023-05-22 22:12:13 +0800

    Added Login and Register together with the css files and js files

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if (isset($_POST['email'])) {
   $email = mysqli_real_escape_string($con, $email);
   $password = stripslashes($_REQUEST['password']);
   $password = mysqli_real_escape_string($con, $password);
-  $query = "SELECT * FROM `users` WHERE email='$email'and password='" . md5($password) . "'";
+  $query = "SELECT * FROM users WHERE email='$email'and password='" . md5($password) . "'";
   $result = mysqli_query($con, $query) or die(mysqli_error($con));
   $rows = mysqli_num_rows($result);
   if ($rows == 1) {
@@ -20,118 +20,76 @@ if (isset($_POST['email'])) {
 } else {
 }
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
+  <head>
+  	<title>TUSTOS Login Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/loginstyle.css">
 
-  <title>Login</title>
+	</head>
+	<body style="background-color: #1E1E1E">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+		      	<div class="icon d-flex align-items-center justify-content-center">
+		      		<span class="fa fa-user-o"></span>
+		      	</div>
+		      	<h3 class="text-center mb-4">Login your Account</h3>
+						<form action="#" class="login-form">
+		      		<div class="form-group">
+		      			<input type="text" name="email" class="form-control rounded-left" placeholder="Email" required>
+		      		</div>
+	            <div class="form-group d-flex">
+	              <input type="password" name="password" class="form-control rounded-left" placeholder="Password" required>
+	            </div>
+	            <div class="form-group d-md-flex">
+	            	<div class="w-50">
+	            		<label class="checkbox-wrap checkbox-primary">Remember Me
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+									</label>
+								</div>
+								<div class="w-50 text-md-right">
+									<a href="#">Forgot Password</a>
+								</div>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" style="border-radius:0%">Login</button>
+	            </div>
+	          </form>
 
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    .login-form {
-      width: 340px;
-      margin: 50px auto;
-      font-size: 15px;
-    }
+			  <br><br><br><br><p class="text-center">Don't have an account?<a href="register.php" class="text-danger"> Register Here</a></p>
+	        </div>			
+				</div>				
+			</div>		
+		</div>	
+	</section>
 
-    .login-form form {
-      margin-bottom: 15px;
-      background: #fff;
-      box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-      padding: 30px;
-      border: 1px solid #ddd;
-    }
-
-    .login-form h2 {
-      color: #636363;
-      margin: 0 0 15px;
-      position: relative;
-      text-align: center;
-    }
-
-    .login-form h2:before,
-    .login-form h2:after {
-      content: "";
-      height: 2px;
-      width: 10%;
-      background: #d4d4d4;
-      position: absolute;
-      top: 50%;
-      z-index: 2;
-    }
-
-    .login-form h2:before {
-      left: 0;
-    }
-
-    .login-form h2:after {
-      right: 0;
-    }
-
-    .login-form .hint-text {
-      color: #999;
-      margin-bottom: 30px;
-      text-align: center;
-    }
-
-    .login-form a:hover {
-      text-decoration: none;
-    }
-
-    .form-control,
-    .btn {
-      min-height: 38px;
-      border-radius: 2px;
-    }
-
-    .btn {
-      font-size: 15px;
-      font-weight: bold;
-    }
-  </style>
-</head>
-
-<body>
-  <div class="login-form">
-    <form action="" method="POST" autocomplete="off">
-      <h2 class="text-center">TUSTOS</h2>
-      <p class="hint-text"></p>
-      <div class="form-group">
-        <input type="text" name="email" class="form-control" placeholder="Email" required="required">
-      </div>
-      <div class="form-group">
-        <input type="password" name="password" class="form-control" placeholder="Password" required="required">
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-success btn-block" style="border-radius:0%;">Login</button>
-      </div>
-      <div class="clearfix">
-        <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
-        
-      </div>
-    </form>
-    <p class="text-center">Don't have an account?<a href="register.php" class="text-danger"> Register Here</a></p>
-  </div>
-</body>
-<!-- Bootstrap core JavaScript -->
-<script src="js/jquery.slim.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<!-- Menu Toggle Script -->
-<script>
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
-</script>
-<script>
-  feather.replace()
-</script>
-
+  <script src="js/ljquery.min.js"></script>
+  <script src="js/lpopper.js"></script>
+  <script src="js/lbootstrap.min.js"></script>
+  <script src="js/lmain.js"></script>
+	</body>
+	<script>
+		$("#menu-toggle").click(function(e) {
+		  e.preventDefault();
+		  $("#wrapper").toggleClass("toggled");
+		});
+	  </script>
+	  <script>
+		feather.replace()
+	  </script>
 </html>
+
--- register.php
+++ register.php
@@ -25,180 +25,71 @@ if (isset($_REQUEST['firstname'])) {
   } else {
     echo ("ERROR: Please Check Your Password & Confirmation password");
   }
-
-  $sql="SELECT * from users where email='$email'";
-
-      $res=mysqli_query($mysqli,$sql);
-
-      if (mysqli_num_rows($res) > 0) {
-        
-        $row = mysqli_fetch_assoc($res);
-        if($email==isset($row['email']))
-        {
-            	echo "email already exists";
-        }
-      }
-
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TUSTOS Register</title>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
 
-  <title>Register</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    body {
-      color: #000;
-      background: #fff;
-      font-family: 'Roboto', sans-serif;
-    }
-
-    .form-control {
-      height: 40px;
-      box-shadow: none;
-      color: #969fa4;
-    }
-
-    .form-control:focus {
-      border-color: #5cb85c;
-    }
-
-    .form-control,
-    .btn {
-      border-radius: 3px;
-    }
-
-    .signup-form {
-      width: 450px;
-      margin: 0 auto;
-      padding: 30px 0;
-      font-size: 15px;
-    }
-
-    .signup-form h2 {
-      color: #636363;
-      margin: 0 0 15px;
-      position: relative;
-      text-align: center;
-    }
-
-    .signup-form h2:before,
-    .signup-form h2:after {
-      content: "";
-      height: 2px;
-      width: 30%;
-      background: #d4d4d4;
-      position: absolute;
-      top: 50%;
-      z-index: 2;
-    }
-
-    .signup-form h2:before {
-      left: 0;
-    }
-
-    .signup-form h2:after {
-      right: 0;
-    }
-
-    .signup-form .hint-text {
-      color: #999;
-      margin-bottom: 30px;
-      text-align: center;
-    }
-
-    .signup-form form {
-      color: #999;
-      border-radius: 3px;
-      margin-bottom: 15px;
-      background: #fff;
-      box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-      padding: 30px;
-      border: 1px solid #ddd;
-    }
-
-    .signup-form .form-group {
-      margin-bottom: 20px;
-    }
-
-    .signup-form input[type="checkbox"] {
-      margin-top: 3px;
-    }
-
-    .signup-form .btn {
-      font-size: 16px;
-      font-weight: bold;
-      min-width: 140px;
-      outline: none !important;
-    }
-
-    .signup-form .row div:first-child {
-      padding-right: 10px;
-    }
-
-    .signup-form .row div:last-child {
-      padding-left: 10px;
-    }
-
-    .signup-form a:hover {
-      text-decoration: none;
-    }
-
-    .signup-form form a {
-      color: #5cb85c;
-      text-decoration: none;
-    }
-
-    .signup-form form a:hover {
-      text-decoration: underline;
-    }
-  </style>
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/regstyle.css">
 </head>
+<body style="background-color: #1E1E1E">
 
-<body>
-  <div class="signup-form">
-    <form action="" method="POST" autocomplete="off">
-      <h2>Register</h2>
-      <div class="form-group">
-        <div class="row">
-          <div class="col"><input type="text" class="form-control" name="firstname" placeholder="First Name" required="required"></div>
-          <div class="col"><input type="text" class="form-control" name="lastname" placeholder="Last Name" required="required"></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <input type="email" class="form-control" name="email" placeholder="Email" required="required">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
-      </div>
-      <div class="form-group">
-        <label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-danger btn-lg btn-block" style="border-radius:0%;">Register</button>
-      </div>
-    </form>
-    <div class="text-center">Already have an account? <a class="text-success" href="login.php">Login Here</a></div>
-  </div>
+    <div class="main">
+
+        <section class="signup">
+            <!-- <img src="images/signup-bg.jpg" alt=""> -->
+            <div class="container">
+                <div class="signup-content">
+                    <form action="" method="POST" autocomplete="off">
+                        <h2 class="form-title">Create account</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="firstname" id="name" placeholder="First Name" required="required"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="lastname" id="name" placeholder="Last Name" required="required"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-input" name="email" id="email" placeholder="Your Email" required="required"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="password" id="password" placeholder="Password" required="required"/>
+                            <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="confirm_password" id="confirm_password" placeholder="Confirm password" required="required"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Sign up"/>
+                        </div>
+                    </form>
+                    <p class="loginhere">
+                        Have already an account ? <a href="login.php" class="loginhere-link">Login here</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/regjquery.min.js"></script>
+    <script src="js/regmain.js"></script>
 </body>
-<!-- Bootstrap core JavaScript -->
-<script src="js/jquery.slim.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<!-- Croppie -->
-<script src="js/profile-picture.js"></script>
-<!-- Menu Toggle Script -->
+
 <script>
   $("#menu-toggle").click(function(e) {
     e.preventDefault();
@@ -208,5 +99,4 @@ if (isset($_REQUEST['firstname'])) {
 <script>
   feather.replace()
 </script>
-
 </html>
