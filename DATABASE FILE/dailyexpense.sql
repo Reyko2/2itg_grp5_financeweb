d3d17b35fc02c91660495b9e2c30fecdfa150659
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-05-23 12:16:04 +0800

    Update dailyexpense.sql

--- DATABASE FILE/dailyexpense.sql
+++ DATABASE FILE/dailyexpense.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2021 at 07:16 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: May 22, 2023 at 11:54 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `budget`
+--
+
+CREATE TABLE `budget` (
+  `budget_id` int(20) NOT NULL,
+  `user_id` varchar(15) NOT NULL,
+  `budget` int(20) NOT NULL,
+  `budgetdate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `budget`
+--
+
+INSERT INTO `budget` (`budget_id`, `user_id`, `budget`, `budgetdate`) VALUES
+(1, '7', 13, '2023-05-23'),
+(2, '7', 25, '2023-05-23'),
+(3, '7', 31, '2023-05-23'),
+(4, '7', 31, '2023-05-23'),
+(6, '7', 100, '2023-05-23');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `expenses`
 --
 
@@ -30,9 +55,20 @@ CREATE TABLE `expenses` (
   `expense_id` int(20) NOT NULL,
   `user_id` varchar(15) NOT NULL,
   `expense` int(20) NOT NULL,
-  `expensedate` varchar(15) NOT NULL,
+  `expensedate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `expensecategory` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`expense_id`, `user_id`, `expense`, `expensedate`, `expensecategory`) VALUES
+(88, '10', 1200, '2023-05-22', 'Bills & Recharges'),
+(95, '7', 24, '2023-05-22', 'Food'),
+(99, '10', 213, '2023-05-22', 'Entertainment'),
+(100, '7', 30, '2023-05-23', 'Food'),
+(104, '7', 100, '2024-10-31', 'Entertainment');
 
 -- --------------------------------------------------------
 
@@ -48,11 +84,25 @@ CREATE TABLE `users` (
   `profile_path` varchar(50) NOT NULL DEFAULT 'default_profile.png',
   `password` varchar(50) NOT NULL,
   `trn_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `profile_path`, `password`, `trn_date`) VALUES
+(7, 'Rico', 'Nieto', 'riconieto@gmail.com', 'a.jpg', '81dc9bdb52d04dc20036dbd8313ed055', '2023-05-11 11:28:36'),
+(10, 'Enrique ', 'Ressurreccion', 'pq12@gmail.com', 'default_profile.png', '202cb962ac59075b964b07152d234b70', '2023-05-22 16:22:28');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `budget`
+--
+ALTER TABLE `budget`
+  ADD PRIMARY KEY (`budget_id`);
 
 --
 -- Indexes for table `expenses`
@@ -71,15 +121,24 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `budget`
+--
+ALTER TABLE `budget`
+  MODIFY `budget_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `expense_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
